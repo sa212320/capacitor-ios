c43a40207ef9401a63ce09e78e67a25d82740139
sa212320 <sa212320@gmail.com>
2024-06-07 14:24:36 +0800

    修正 16.4 之後無法 debug 的問題

--- Capacitor/Capacitor/CAPBridgeViewController.swift
+++ Capacitor/Capacitor/CAPBridgeViewController.swift
@@ -296,6 +296,12 @@ extension CAPBridgeViewController {
             aWebView.backgroundColor = UIColor.systemBackground
             aWebView.scrollView.backgroundColor = UIColor.systemBackground
         }
+        #if DEBUG
+            if #available(iOS 16.4, *) {
+                aWebView.isInspectable = true
+            }
+        #endif
+                
         aWebView.capacitor.setKeyboardShouldRequireUserInteraction(false)
         // set our ivar
         webView = aWebView
